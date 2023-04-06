bfdc44a730af41c6616843739068b9675efadce1
whoisKyleFrench <kbfrench88@gmail.com>
2023-04-06 17:33:35 -0400

    Add function to delete task, create delete button

--- todo_list_app/todo.py
+++ todo_list_app/todo.py
@@ -4,21 +4,45 @@ tasks = {} # Initialize dictionary
 task_id = 1 # Intialize task_id
 
 # Function to add a task
-def add_task() :
-    global task_id
+def add_task():
+    global task_id # Make task_id a global variable
     task_description = task_text_box.get() # Get the user input
-    tasks[task_id] = f"{task_description}\n" # Add the task to the dictionary
+    tasks[task_id] = task_description # Add the task to the dictionary
     task_id += 1
     update_task_list() # Update the task list display
     task_text_box.delete(0, tk.END) # Delete the text in task_text_box
 
+
+def delete_task():
+    # Get the index of the selected line
+    index = task_list_display.curselection()
+
+    if index:
+        # Get the task_description from the index of the selected line
+        task_description = task_list_display.get(index[0])
+
+        # Find the task in the dictionary by its description
+        task_id_to_remove = None
+        for task_id, description in tasks.items():
+            if description == task_description:
+                task_id_to_remove = task_id
+                break
+
+        if task_id_to_remove is not None:
+            # Remove the task from the dictionary
+            del tasks[task_id_to_remove]
+            # Update the task list display
+            update_task_list()
+
+
+
 # Function to update the task list display
 def update_task_list() :
-    task_list_display.delete(1.0, tk.END) # Clear the task list
+    task_list_display.delete(0, tk.END) # Clear the task list
 
     # Iterate over the tasks and add them to the list display
     for task_id, task_description in tasks.items() :
-        task_text = f"{task_id}. {task_description}"
+        task_text = task_description
         task_list_display.insert(tk.END, task_text)
 
 
@@ -34,7 +58,7 @@ title_label = tk.Label(root, text="To-Do List", font=("Arial", 18))
 title_label.pack(pady=5)
 
 # Create the task list display
-task_list_display = tk.Text(root, width=20, height=10)
+task_list_display = tk.Listbox(root, width=20, height=10)
 task_list_display.pack(padx=5, pady=10)
 
 entry_label = tk.Label(root, text="Enter tasks below", font=("Arial", 10))
@@ -45,8 +69,12 @@ task_text_box = tk.Entry(root, width=26)
 task_text_box.pack(pady=10)
 
 # Create the button for adding tasks
-add_task_button = tk.Button(root, text="Add Task", width=10, command=add_task)
+add_task_button = tk.Button(root, text="Add Task", width=10, command=lambda: add_task())
 add_task_button.pack(pady=10)
+
+# Create the delete button for deleting tasks
+delete_task_button = tk.Button(root, text="Delete Task", width=10, command=delete_task)
+delete_task_button.pack(pady=10)
 
 # Start the main event loop
 root.mainloop()
