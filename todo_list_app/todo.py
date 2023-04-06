bad05b91dca97f2d93334018b1f9e5fa9f0b4e78
whoisKyleFrench <kbfrench88@gmail.com>
2023-04-06 15:54:42 -0400

    create function to add task

--- todo_list_app/todo.py
+++ todo_list_app/todo.py
@@ -1,4 +1,12 @@
 import tkinter as tk
 
 tasks = {} # Initialize dictionary
-task_id = 1 # Intialize task id
+task_id = 1 # Intialize task_id
+
+# Function to add a task
+def add_task() :
+    global task_id
+    task_description = task_text_box.get() # Get the user input
+    tasks[task_id] = f"{task_description}\n" # Add the task to the dictionary
+    task_id += 1
+    update_task_list() # Update the task list display
