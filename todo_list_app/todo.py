acc9bde8211b3668f5831019b727e253fecf8764
whoisKyleFrench <kbfrench88@gmail.com>
2023-04-06 15:55:25 -0400

    create function to update task list

--- todo_list_app/todo.py
+++ todo_list_app/todo.py
@@ -10,3 +10,12 @@ def add_task() :
     tasks[task_id] = f"{task_description}\n" # Add the task to the dictionary
     task_id += 1
     update_task_list() # Update the task list display
+
+# Function to update the task list display
+def update_task_list() :
+    task_list_display.delete(1.0, tk.END) # Clear the task list
+
+    # Iterate over the tasks and add them to the list display
+    for task_id, task_description in tasks.items() :
+        task_text = f"{task_id}. {task_description}"
+        task_list_display.insert(tk.END, task_text)
