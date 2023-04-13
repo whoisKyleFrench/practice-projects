d4f15ad6a2a0f275ce5cbe9385bf56d7ee3db717
whoisKyleFrench <kbfrench88@gmail.com>
2023-04-13 18:06:19 -0400

    Create label, button and calendar

--- apps/alarm_clock_app/alarm.py
+++ apps/alarm_clock_app/alarm.py
@@ -2,7 +2,43 @@ import time
 import datetime
 import tkinter as tk
 from tkinter import *
+from tkcalendar import Calendar
 from playsound import playsound
+
+
+def show_calendar():
+    top = Toplevel(root)
+    cal = Calendar(top, selectmode="day")
+    cal.pack()
+
+    # Get the main window's position and size
+    main_window_x = root.winfo_x()
+    main_window_y = root.winfo_y()
+    main_window_width = root.winfo_width()
+    main_window_height = root.winfo_height()
+
+    # Calculate the calendar window position
+    calendar_width = top.winfo_reqwidth()
+    calendar_height = top.winfo_reqheight()
+    x_position = main_window_x + (main_window_width // 2) - (calendar_width // 2)
+    y_position = main_window_y + (main_window_height // 2) - (calendar_height // 2)
+
+    # Set the calendar window position
+    top.geometry(f"+{x_position}+{y_position}")
+
+    def get_date():
+        global selectedDate, emptyDate
+        selectedDate = cal.selection_get()
+        # Hide the "Select date" button
+        selectDateButton.pack_forget()
+        # Destroy the calendar window
+        top.destroy()
+        return selectedDate
+
+    # Create the OK button
+    button = Button(top, text="OK", command=get_date)
+    button.pack()
+
 
 # Create the GUI window
 root = tk.Tk()
@@ -22,6 +58,23 @@ editmenu.add_command(label="Add Alarm", command="addAlarm")
 editmenu.add_command(label="Delete Alarm", command="delAlarm")
 menubar.add_cascade(label="Edit", menu=editmenu)
 root.config(menu=menubar)
+
+# Create a label
+var = StringVar()
+label = Label(root, textvariable=var, font=("Arial", 16))
+var.set("Please select a date and time for the alarm")
+label.pack(pady="10")
+
+# Create the calendar button
+selectDateButton = Button(
+    root,
+    text="Select date",
+    font=("Arial", 10),
+    command=show_calendar,
+    height="2",
+    width="10",
+)
+selectDateButton.pack()
 
 
 def main():
