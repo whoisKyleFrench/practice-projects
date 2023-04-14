2027d8a09d9b2ab19f7b826f4ab0062ec7c04cdd
whoisKyleFrench <kbfrench88@gmail.com>
2023-04-14 18:15:19 -0400

    Welcome the user to the program

---
+++ programs/madlib_gen/madlibs.py
@@ -0,0 +1,3 @@
+print(
+    "\nWelcome to MLG, the Mad Libs Generator program!\n\nLet's create a silly story!\nI'll ask you for some nouns, verbs and adjectives and we'll use those to generate a silly sentence.\n\nLet's begin!"
+)
