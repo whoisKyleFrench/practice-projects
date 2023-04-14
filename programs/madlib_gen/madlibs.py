d736b0e460f0184d607b37c5a5f6eeaa15eadaae
whoisKyleFrench <kbfrench88@gmail.com>
2023-04-14 18:18:17 -0400

    Setup grammar handling

--- programs/madlib_gen/madlibs.py
+++ programs/madlib_gen/madlibs.py
@@ -10,3 +10,11 @@ verb_input = input("\nEnter verb: ")
 print("Nice!")
 adjective_input = input("\nEnter an adjective: ")
 print("\nPerfect! That's it!\nHere is your silly story:")
+
+# Setup grammar handling
+a_an = ["a", "an"]
+options = ("a", "e", "i", "o", "u")
+if noun_input.startswith(options):
+    a_or_an = a_an[1]
+else:
+    a_or_an = a_an[0]
