57379d4880cb411b3db2663faf0c8c5bc4e8d1dc
whoisKyleFrench <kbfrench88@gmail.com>
2023-04-14 18:17:02 -0400

    Get the user input

--- programs/madlib_gen/madlibs.py
+++ programs/madlib_gen/madlibs.py
@@ -1,3 +1,12 @@
+# Welcome the user to the program
 print(
     "\nWelcome to MLG, the Mad Libs Generator program!\n\nLet's create a silly story!\nI'll ask you for some nouns, verbs and adjectives and we'll use those to generate a silly sentence.\n\nLet's begin!"
 )
+
+# Get the user input
+noun_input = input("\nEnter a noun: ")
+print("Good one!")
+verb_input = input("\nEnter verb: ")
+print("Nice!")
+adjective_input = input("\nEnter an adjective: ")
+print("\nPerfect! That's it!\nHere is your silly story:")
